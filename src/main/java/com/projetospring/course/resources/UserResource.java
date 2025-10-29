7f5cf1fd4ea94b3f610b2b89299f81f2c51aee22
wan.vercosa@gmail.com <wan.vercosa@gmail.com>
2025-10-29 09:50:23 -0300

    user entity and resource

---
+++ src/main/java/com/projetospring/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projetospring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetospring.course.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Joana", "Joana@gmail.com", "7777", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
